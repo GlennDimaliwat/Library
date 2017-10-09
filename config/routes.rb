a02564ce83fb20c231cd3a0ccbdcaa04f7d11496
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-10-09 16:19:50 +1100

    Add Books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # get 'books/index'
+
+  # get 'books/show'
+
+  # get 'books/new'
+
+  # get 'books/create'
+
+  # get 'books/edit'
+
+  # get 'books/update'
+
+  # get 'books/destroy'
+
+  root 'books#index'
+  resources 'books'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
